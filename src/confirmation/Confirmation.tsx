0748833ec51e879b6d961796ab99dea355274262
Simon Pringle-Wallace <s.pringlewallace@gmail.com>
2022-10-21 21:02:00 -0400

    Add Confirmation Page

--- src/confirmation/Confirmation.tsx
+++ src/confirmation/Confirmation.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import meditate from '../../src/assets/sammy_meditation.png'
+import Header from '../header/header';
+import './confirmation.css';
 
 const Confirmation = () => {
     return (
-        <div>Success!!</div>
+        <>
+            <Header withCart={false}/>
+            <div className='confirmation-container'>
+                <h1>Thank you for your order!</h1>
+                <img className='meditation' src={meditate}/>
+                <h2>We'll take it from here. Your trash tags are on the way.</h2>
+                <h3>Questions?<br/>Email us at village-tags@gmail.com</h3>
+            </div>
+        </>
     )
 }
 
--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import { Icon } from 'semantic-ui-react';
+import { ITag } from '../Home/interfaces';
 import { IOrder } from '../interfaces';
 import './header.css'
 
@@ -12,8 +14,9 @@ const style = {
 }
 
 interface HeaderProps {
-    order: IOrder;
-    onCartClick(): void;
+    withCart?: boolean;
+    order?: IOrder;
+    onCartClick?(): void;
 }
 
 const cartStyle = {
@@ -23,10 +26,12 @@ const cartStyle = {
     }
 }
 
-const Header = ({ order, onCartClick }: HeaderProps) => {
-
+const Header = ({ order, onCartClick, withCart=true }: HeaderProps) => {
+const navigate = useNavigate();
     const getCartNumber = () => {
-
+        if(!order) {
+            return '0';
+        }
         const cartItems = Object.values(order).reduce((acc, curr) => acc + curr.quantity, 0)
         
         if(cartItems === 0) {
@@ -37,11 +42,11 @@ const Header = ({ order, onCartClick }: HeaderProps) => {
     const cartNumber = getCartNumber()
     return (
         <div className='header-container'>
-            <div className='home-icon'>
-                Red Hook Village Trash Stickers
+            <div className='home-icon' onClick={() => navigate('/')}>
+                Red Hook Village Trash Tags
             </div>
             <div style={cartStyle} onClick={onCartClick}>
-                <Icon size='large' style={{cursor: 'pointer'}} name="shopping cart"></Icon>
+                {withCart && <Icon size='large' style={{cursor: 'pointer'}} name="shopping cart"></Icon>}
                 {(parseInt(cartNumber) > 0) && <div style={{backgroundColor: 'red', height: '15px', width: '15px', borderRadius: '10px', marginTop: '-3px', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '10px'}}>
                     {getCartNumber()}
                 </div>}
